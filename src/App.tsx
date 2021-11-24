64905db0dc0d0831a79853bdd7d43c09209b12ed
swizzlestix09 <angely.rodriguez@gmail.com>
2021-11-24 14:39:16 -0500

    reciving cryptoList re axios req

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import CryptoMenu from './Menu/CryptoMenu';
 import axios from 'axios';
 import './App.css';
+const getCryptoURL = 'https://api.exchange.coinbase.com/currencies';
 
 const App = () => {
-  const [currencies, getCurrencies] = React.useState();
+  const [currencies, getCurrencies] = React.useState(null);
 
-  React.useEffect( ()=> {
-
-  });
+  React.useEffect(() => {
+    axios.get(getCryptoURL)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, []);
 
   return (
     <div className="App">
-     <CryptoMenu />
+      <CryptoMenu />
     </div>
   );
 }
