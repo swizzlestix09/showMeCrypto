18e646d06f9df4367b3907f373505ecae229b17d
swizzlestix09 <angely.rodriguez@gmail.com>
2021-11-25 12:21:06 -0500

    fixed props issue inside functional component-cryptomenu

--- src/Menu/CryptoMenu.tsx
+++ src/Menu/CryptoMenu.tsx
@@ -1,7 +1,12 @@
 import CryptoItem  from "./CryptoItem";
 
 //might need to add proptype to this..
-const CryptoMenu = () => {
+export interface Props {
+  currencies: any[]
+};
+
+const CryptoMenu = (props: Props) => {
+  console.log('props?', props)
   return (
     <div className="CryptoMenu">
       <p>ohmergosh, CryptoMenu is live</p>
