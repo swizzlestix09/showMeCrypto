922da2f0b21efa5e33f306b5dfa9a50061c81c5e
swizzlestix09 <angely.rodriguez@gmail.com>
2021-11-27 14:34:04 -0500

    working onclick function

--- src/Menu/CryptoItem.tsx
+++ src/Menu/CryptoItem.tsx
@@ -1,19 +1,30 @@
-
+import * as React from 'react';
+import CryptoInfo from '../CryptoDetails/CryptoInfo';
 //going to use material ui componenet here - perhaps paper
 export interface Props {
-  tokenId: string,
-  tokenName: string,
-  tokenSym: string
+  tokenId?: string,
+  tokenName?: string,
+  tokenSym?: string,
+  clickCurrency?: any
 };
 
-const CryptoItem= (props: Props) => (
-  <>
-    <li className="eachCryptoc">
-      <abbr>{props.tokenId} </abbr>
-      <strong> {props.tokenName} </strong>
-      {props.tokenSym === null ? '' : props.tokenSym}
-    </li>
-  </>
-);
+const CryptoItem = (props: Props) => {
+  const [eachCurrency, getCurrencyInfo] = React.useState<string | null>(null);
+
+  function clickCurrency(currencyId: any) {
+    console.log('in click', currencyId.target.innerHTML);
+    getCurrencyInfo(currencyId.target.innerHTML);
+  };
+  console.log('each currency ', eachCurrency)
+  return (
+    <>
+      <li className="eachCryptoc" value={props.tokenId} onClick={(e) => clickCurrency(e)}>
+        <strong>{props.tokenId} </strong>
+        {props.tokenName}
+        {props.tokenSym === null ? '' : props.tokenSym}
+      </li>
+    </>
+  );
+};
 
 export default CryptoItem;
