8122ad567a18fc1fd108fe886a68dbf5ded88788
swizzlestix09 <angely.rodriguez@gmail.com>
2021-12-05 11:16:10 -0500

    static serve

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('./', express.static('dist'));
 
 const port = process.env.PORT || 3002;
 
