3e9668ca8ce1dd25451c8fa8c35a1fbda5439ed8
swizzlestix09 <angely.rodriguez@gmail.com>
2021-12-06 11:03:20 -0500

    Revert "sad"
    This reverts commit 960de22b747cb99e82cea432d087f614473c5203.

---
+++ src/Menu/CryptoItem.tsx
@@ -0,0 +1,19 @@
+import MenuItem from '@material-ui/core/MenuItem';
+export interface Props {
+  tokenId: string,
+  tokenName: string,
+  tokenSym: string,
+  clickCurrency?: any
+};
+
+const CryptoItem = (props: Props) => {
+  return (
+    <>
+      <MenuItem className="cryptoparam" value={props.tokenId} onClick={(e) => props.clickCurrency( e.target )}>
+          {`${props.tokenId} `}
+      </MenuItem>
+    </>
+  );
+};
+
+export default CryptoItem;
---
+++ src/Menu/CryptoMenu.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import CryptoItem from "./CryptoItem";
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import Typography from '@mui/material/Typography';
+export interface Props {
+  currencies: any[],
+  clickCurrency: any
+};
+
+const CryptoMenu = (props: Props) => {
+  let listOfCrypto = props.currencies.map(token => {
+    return (
+      <CryptoItem key={token.id} tokenId={token.id} tokenName={token.name} tokenSym={token.symbol} clickCurrency={props.clickCurrency} />
+    );
+  })
+
+  return (
+    <>
+      <Paper sx={{ maxWidth: 'fit-content', padding: '20px 20px' }}>
+        <h3>CryptoCurrencies</h3>
+        <Typography component={'span'} noWrap>
+          <MenuList dense style={{maxHeight: 800, overflow: 'auto'}}>
+            {listOfCrypto}
+          </MenuList>
+        </Typography>
+      </Paper>
+    </>
+  )
+};
+
+export default CryptoMenu;
